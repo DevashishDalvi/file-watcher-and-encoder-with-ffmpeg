dbcafad76250e93826d04c891bec030a87140092
Devashish Dalvi <92207754+DevashishDalvi@users.noreply.github.com>
2026-02-10 14:06:51 +0530

    Code update: refactoring code(Home)

--- test01.py
+++ test01.py
@@ -3,25 +3,28 @@ import datetime
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
-now = datetime.datetime.now()
 
 
 class WatcherHandler(FileSystemEventHandler):
+    def log(self, msg):
+        now = datetime.datetime.now()
+        print(f"[{now:%I:%M %p}] {msg}")
+
     # Triggered when a file or directory is modified
     def on_modified(self, event):
-        print(f"[{now:%I:%M %p}]Modified: {event.src_path}")
+        self.log(f"Modified: {event.src_path}")
 
     # Triggered when a new file or directory is created
     def on_created(self, event):
-        print(f"[{now:%I:%M %p}]Created: {event.src_path}")
+        self.log(f"Created: {event.src_path}")
 
     # Triggered when a file or directory is deleted
     def on_deleted(self, event):
-        print(f"[{now:%I:%M %p}]Deleted: {event.src_path}")
+        self.log(f"Deleted: {event.src_path}")
 
     # Triggered when a file or directory is moved/renamed
     def on_moved(self, event):
-        print(f"[{now:%I:%M %p}]Moved: {event.src_path} to {event.dest_path}")
+        self.log(f"Moved: {event.src_path} to {event.dest_path}")
 
 
 if __name__ == "__main__":
