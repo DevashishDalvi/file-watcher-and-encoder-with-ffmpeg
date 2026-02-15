84208307691da3ec3c1b7a015d01384af786dccd
DATA <data@DATA2.local>
2026-02-15 10:00:28 +0530

    Commit: code commit

--- test01.py
+++ test01.py
@@ -1,49 +1,20 @@
+import datetime
+import os
 import time
-from datetime import datetime as dt
-from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
-from watchdog.observers.polling import PollingObserver as Observer
+from pathlib import Path
 
-
-
-class WatcherHandler(FileSystemEventHandler):
-    def log(self, msg):
-        now = dt.now()
-        print(f"[{now:%I:%M %p}] {msg}")
-
-    # Triggered when a file or directory is modified
-    def on_modified(self, event):
-        self.log(f"Modified: {event.src_path}")
-
-    # Triggered when a new file or directory is created
-    def on_created(self, event):
-        self.log(f"Created: {event.src_path}")
-
-    # Triggered when a file or directory is deleted
-    def on_deleted(self, event):
-        self.log(f"Deleted: {event.src_path}")
-
-    # Triggered when a file or directory is moved/renamed
-    def on_moved(self, event):
-        self.log(f"Moved: {event.src_path} to {event.dest_path}")
-
-
-if __name__ == "__main__":
-    # Set the path to the folder you want to watch
-    path = "/Volumes/FLAME_MEDIA/OUTPUT"  # Change this to your folder path
-
-    event_handler = WatcherHandler()
-    observer = Observer()
-    observer.schedule(event_handler, path, recursive=True)
-
-    print(f"Now watching: {path}")
-    observer.start()
-
+def get_mtime(path):
     try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        observer.stop()
-        print("\nStopped watching.")
+        return os.path.getmtime(path)
+    except OSError as e:
+        return -1
 
-    observer.join()
+def log(msg, mode:str = "Screen"):
+    mode = mode.lower()
+    now = datetime.datetime.now()
+    if mode in ["screen", "file"]:
+        if mode == "screen":
+            print(f"[{now: %I:%M %p}]{msg}", flush=True)
+        else:
+            pass
+
